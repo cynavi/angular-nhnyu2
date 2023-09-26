095713620852b265dd94543cb0ca23ea5bcc3e96
kajiyy <basnetavinay@gmail.com>
2023-09-26 22:07:51 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nhnyu2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nhnyu2)
---
+++ src/app/example.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  templateUrl: 'example.component.html',
+  styleUrls: ['example.component.css'],
+})
+export class ExampleComponent {
+  myDate: any;
+
+  showdate() {
+    console.log(this.myDate);
+  }
+}
